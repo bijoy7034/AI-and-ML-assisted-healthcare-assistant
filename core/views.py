93340cfa6e454a435bb6b377d44ae8d23ccf3989
Bijoy Anil <86096110+bijoy7034@users.noreply.github.com>
2024-01-07 14:03:53 +0530

    ui updated

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
+from .models import Medical, User, Ment, Profile
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import auth
+import numpy as np
+import os
+from django.contrib import messages
+import joblib as joblib
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
+
+def home(request):
+	return render(request, 'home.html')
+
+def registerView(request):
+	return render(request, 'register.html')
