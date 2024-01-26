00f16e2fec901ef5e2b3d25f697d94c306454df9
Bijoy Anil <86096110+bijoy7034@users.noreply.github.com>
2024-01-26 12:04:59 +0530

    profile created

--- core/urls.py
+++ core/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
 path('', views.home, name='home'),
 path('register/', views.registerView, name='reg'),
 path('reg_user/', views.registerUser, name='reg_user'),
-
+path('login/', views.loginView, name='login'),
+path('patient/', views.patient_home, name='patient'),
+path('create_profile/', views.create_profile, name='create_profile'),
 ]
--- core/views.py
+++ core/views.py
@@ -32,3 +32,64 @@ def registerUser(request):
 	else:
 		messages.error(request, 'Failed To Register, Try Again Later')
 		return redirect('reg')
+
+def loginView(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None and user.is_active:
+			auth.login(request, user)
+			if user.is_patient:
+				return redirect('patient')
+			elif user.is_doctor:
+				return redirect('doctor')
+			else:
+				return redirect('login')
+		else:
+			messages.info(request, "Invalid Username Or Password")
+			return redirect('login')
+	else:
+		return render(request, 'login.html')					
+
+
+def patient_home(request):
+	doctor = User.objects.filter(is_doctor=True).count()
+	patient = User.objects.filter(is_patient=True).count()
+	appointment = Ment.objects.filter(approved=True).count()
+	medical1 = Medical.objects.filter(medicine='See Doctor').count()
+	medical2 = Medical.objects.all().count()
+	medical3 = int(medical2) - int(medical1)
+
+	user_id = request.user.id
+	user_profile = Profile.objects.filter(user_id=user_id)
+	if not user_profile:
+		context = {'profile_status':'Please Create Profile To Continue', 'doctor':doctor, 'ment':appointment, patient:'patient', 'drug':medical3}
+		return render(request, 'patient/home.html', context)
+	else:
+		context = {'status':'1', 'doctor':doctor, 'ment':appointment, patient:'patient', 'drug':medical3}
+		return render(request, 'patient/home.html', context)
+
+
+def create_profile(request):
+	if request.method == 'POST':
+		birth_date = request.POST['birth_date']
+		region = request.POST['region']
+		country = request.POST['country']
+		gender = request.POST['gender']
+		user_id = request.user.id
+
+		Profile.objects.filter(id = user_id).create(user_id=user_id, birth_date=birth_date, gender=gender, region=region)
+		messages.success(request, 'Your Profile Was Created Successfully')
+		return redirect('patient')
+	else:
+		user_id = request.user.id
+		users = Profile.objects.filter(user_id=user_id)
+		users = {'users':users}
+		choice = ['1','0']
+		gender = ["Male", "Female"]
+		context = {"users": {"users":users}, "choice":{"choice":choice}, "gender":gender}
+		return render(request, 'patient/create_profile.html', context)	
+
+
+
